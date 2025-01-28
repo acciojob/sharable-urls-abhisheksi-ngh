321309f2fd21579c712d60c1d360f2ccb1a9759c
Abhishek Singh <abhishekkumarsingh9585@gmail.com>
2025-01-28 22:03:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // your code here
+document.getElementById(`urlform`).addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const name= document.getElementById('name').ariaValueMax;
+    const year= document.getElementById('year').ariaValueMax;
+
+    let url = 'https://localhost:8080/';
+
+    if(name && year){
+        url+=`?name=${encodeURIComponent(name)}&year=${year}`;
+
+    }else if(name){
+        url+=`?name=${encodeURIComponent(name)}`;
+    }else if(year){
+        url+=`?year=${year}`;
+    }
+    document.getElementById(`url`).textContent=url;
+
+});
